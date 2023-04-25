14fd86923c48b3708f23a059ad2d4249c142a29a
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-25 16:21:48 +0530

    Update client.py

--- TeamLegend/core/client.py
+++ TeamLegend/core/client.py
@@ -7,8 +7,8 @@ from telethon import TelegramClient
 from TeamLegend.Config import *
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-legendtbot = TelegramClient("Kannadiga", API_ID, API_HASH)
+legendtbot = TelegramClient("Group_t_Bot", API_ID, API_HASH)
 
-legendpbot = Client("KannadigaBot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+legendpbot = Client("Group_p_Bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
