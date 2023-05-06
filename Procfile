f33d3c7639fd61ca076613c7eb6eb24b944e42bf
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-06 09:15:39 +0000

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python3 -m TeamLegend
+worker: python3 -m KannadigaBot
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Team Legend
+:: Enables virtual env mode and then starts kannaidga
+env\scripts\activate.bat && py -m KannadigaBot
