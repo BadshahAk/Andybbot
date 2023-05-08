23ac2d64f14fdb7ab444984ad8dd1060e3943b1e
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-09 00:43:07 +0530

    Delete TeamLegend directory

--- TeamLegend/plugins/admins.py
+++
@@ -1,7 +0,0 @@
-import html
-import os
-
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
-from telegram.error import BadRequest
-from telegram.ext import CallbackContext, CommandHandler, run_async
-from telegram.utils.helpers import mention_html
