5477e8fe70f99d0527676e89c282a8fa08d249ef
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-09 08:49:27 +0530

    Add files via upload

--- TeamLegend/Config/__init__.py
+++ TeamLegend/Config/__init__.py
@@ -1,13 +1,46 @@
 import os
+import sys
+import time
 
-from dotenv import load_dotenv
+import telegram.ext as tg
+from aiohttp import ClientSession
+from pyrogram import Client, errors
+from telethon import TelegramClient
+from .core.clients import *
+from .Config import *
+StartTime = time.time()
 
-if os.path.exists(".env"):
-    load_dotenv(".env")
 
-ENV = bool(os.environ.get("ENV", False))
+# if version < 3.6, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGS.error(
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    )
+    quit(1) 
 
-if ENV or os.path.exists(".env"):
-    from sample_config import *  # noqa
-elif os.path.exists("config.py"):
-    from config import *  # noqa
+DRAGONS.add(OWNER_ID)
+DEV_USERS.add(OWNER_ID)
+
+print("[INFO]: Getting Bot Info...")
+
+BOT_ID = dispatcher.bot.id
+BOT_NAME = dispatcher.bot.first_name
+BOT_USERNAME = dispatcher.bot.username
+
+DRAGONS = list(DRAGONS) + list(DEV_USERS)
+DEV_USERS = list(DEV_USERS)
+WOLVES = list(WOLVES)
+DEMONS = list(DEMONS)
+TIGERS = list(TIGERS)
+
+# Load at end to ensure all prev variables have been set
+from TeamLegend.modules.helper_funcs.handlers import (
+    CustomCommandHandler,
+    CustomMessageHandler,
+    CustomRegexHandler,
+)
+
+# make sure the regex handler can take extra kwargs
+tg.RegexHandler = CustomRegexHandler
+tg.CommandHandler = CustomCommandHandler
+tg.MessageHandler = CustomMessageHandler
