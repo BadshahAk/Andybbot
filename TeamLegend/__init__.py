68270e0420c5de41962ab32f31b17a1394ddaeac
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-10 04:59:54 +0000

    Update __init__.py

--- TeamLegend/__init__.py
+++ TeamLegend/__init__.py
@@ -1,1 +1,45 @@
+
+import sys
 import time
+from telegram import ext as tg
+from aiohttp import ClientSession
+from pyrogram import Client, errors
+from telethon import TelegramClient
+from TeamLegend.core.clients import *
+from TeamLegend.Config import *
+StartTime = time.time()
+
+
+# if version < 3.6, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGS.error(
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    )
+    quit(1) 
+
+DRAGONS.add(OWNER_ID)
+DEV_USERS.add(OWNER_ID)
+
+print("[INFO]: Getting Bot Info...")
+
+BOT_ID = dispatcher.bot.id
+BOT_NAME = dispatcher.bot.first_name
+BOT_USERNAME = dispatcher.bot.username
+
+DRAGONS = list(DRAGONS) + list(DEV_USERS)
+DEV_USERS = list(DEV_USERS)
+WOLVES = list(WOLVES)
+DEMONS = list(DEMONS)
+TIGERS = list(TIGERS)
+
+# Load at end to ensure all prev variables have been set
+from TeamLegend.helpers.handlers import (
+    CustomCommandHandler,
+    CustomMessageHandler,
+    CustomRegexHandler,
+)
+
+# make sure the regex handler can take extra kwargs
+tg.RegexHandler = CustomRegexHandler
+tg.CommandHandler = CustomCommandHandler
+tg.MessageHandler = CustomMessageHandler
