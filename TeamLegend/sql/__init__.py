c497a36cf165e6b11f8eb77592be27df5d1eb5c2
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-10 10:48:35 +0530

    Update __init__.py

--- TeamLegend/sql/__init__.py
+++ TeamLegend/sql/__init__.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from TeamLegend import DB_URI
-from TeamLegend import LOGGER as log
+from TeamLegend.Config import DB_URI
+from TeamLegend.core.clients import LOGS as log
 
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
