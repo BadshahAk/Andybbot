8b6c6feaa43094dc024d9a1b5ad6c1da62e944c3
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-13 06:39:15 +0530

    Update clients.py

--- TeamLegend/core/clients.py
+++ TeamLegend/core/clients.py
@@ -8,10 +8,10 @@ import logging
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()], level=logging.INFO)
 
-logging.getLOGS("apscheduler").setLevel(logging.ERROR)
-logging.getLOGS("telethon").setLevel(logging.ERROR)
-logging.getLOGS("pyrogram").setLevel(logging.ERROR)
-LOGS = logging.getLOGS("__name__")
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+LOGS = logging.getLogger("__name__")
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 legendtbot = TelegramClient("Group_t_Bot", API_ID, API_HASH)
