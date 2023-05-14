4827b1389ff110a6fab3a04caba7616c5f93f419
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-14 05:48:54 +0530

    Update __init__.py

--- TeamLegend/helpers/legendtbot/__init__.py
+++ TeamLegend/helpers/legendtbot/__init__.py
@@ -1,7 +1,7 @@
 from TeamLegend.Config import DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 from TeamLegend.core.clients import legendtbot
 
-IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
+#IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
 
 IMMUNE_USERS = (
     list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS)
