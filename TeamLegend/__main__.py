54b5d8f000ab61eafd1138263abbb08eabc3c736
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-05-15 18:42:07 +0530

    Update __main__.py

--- TeamLegend/__main__.py
+++ TeamLegend/__main__.py
@@ -163,7 +163,7 @@ def start(update: Update, context: CallbackContext):
             update.effective_message.reply_photo(
                 START_IMG,
                 caption=f"Hello", #.format(escape_markdown(first_name), BOT_NAME),
-                reply_markup=InlineKeyboardMarkup(buttons),
+                #reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
             )
     else:
