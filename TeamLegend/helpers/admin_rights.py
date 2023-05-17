6ac98564667cd33bd7432b8d306140d1d6fdabe8
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-17 16:40:45 +0530

    Update admin_rights.py

--- TeamLegend/helpers/admin_rights.py
+++ TeamLegend/helpers/admin_rights.py
@@ -11,7 +11,7 @@ def user_can_ban(chat: Chat, user: User, bot_id: int) -> bool:
 
 
 def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).can_pin_messages
+    return chat.get_member(OWNER_ID).can_pin_messages
 
 #This is used to Check right of Change Info of OWNER_ID
 def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
