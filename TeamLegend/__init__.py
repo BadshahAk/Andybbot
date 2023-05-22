061fc594aea8087111ffdfad608a650d7f8b7b17
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-05-22 22:47:06 +0530

    Update __init__.py

--- TeamLegend/__init__.py
+++ TeamLegend/__init__.py
@@ -21,10 +21,10 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
 
 
 print("[INFO]: Getting Bot Info...")
-GET_ME = dispatcher.bot.get_me()
-BOT_ID = GET_ME.id
-BOT_NAME = GET_ME.first_name
-BOT_USERNAME = GET_Me.username
+GET_ME = await dispatcher.bot.get_me()
+BOT_ID = await GET_ME.id
+BOT_NAME = await GET_ME.first_name
+BOT_USERNAME = await GET_Me.username
 
 
 
