87074630314a1dcae3eddf0f7811817e74b12939
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-06-01 08:20:12 +0530

    Add files via upload

--- TeamLegend/core/clients.py
+++ TeamLegend/core/clients.py
@@ -1,4 +1,4 @@
-from telegram.ext import Application
+from telegram import ext as tg
 from aiohttp import ClientSession
 from pyrogram import Client, errors
 from telethon import TelegramClient
@@ -13,9 +13,9 @@ logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGS = logging.getLogger("__name__")
 
-updater = Application.builder().token('TOKEN').build()
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 legendtbot = TelegramClient("Group_t_Bot", API_ID, API_HASH)
-dispatcher = updater
-legendpbot = Client("Group_p_Bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
+legendpbot = Client("Group_p_Bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
