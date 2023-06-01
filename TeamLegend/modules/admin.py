412385167afd9e4026b71c0ada223ad74435f64f
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-06-01 08:55:20 +0530

    Update admin.py

--- TeamLegend/modules/admin.py
+++ TeamLegend/modules/admin.py
@@ -352,7 +352,10 @@ def fullpromote(update: Update, context: CallbackContext) -> str:
     chat = update.effective_chat
     user = update.effective_user
     promoter = chat.get_member(user.id)
-    if str(user.id) != OWNER_ID:
+    if str(user.id) not in OWNER_ID:
+        print(OWNER_ID)
+        print(str(user.id))
+                  
         return message.reply_text("➢ Owners have Only Permission to full Promote anyone!")
         
 
